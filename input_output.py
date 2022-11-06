322748129c3a7d35a78fa7663220c72ff8a33ce1
dishanshu .Jagtap <dishanshujagtap@gmail.com>
2022-11-06 16:03:51 +0530

    "python file append and read"

--- input_output.py
+++ input_output.py
@@ -1,1 +1,9 @@
 # Python Input Output
+
+
+with open("text_file_input_output.txt", 'a') as file:
+    txt = file.write("qwerty!!!")
+    file.close()
+ 
+file_read = open("text_file_input_output.txt", 'r')
+print(file_read.read())
