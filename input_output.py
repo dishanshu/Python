12e4f54a15678245f328f0885b49b3b0466cad10
dishanshu .Jagtap <dishanshujagtap@gmail.com>
2022-11-06 16:21:32 +0530

    added comment

--- input_output.py
+++ input_output.py
@@ -1,8 +1,11 @@
 # Python Input Output
 
+
+# Append to a file
 with open("text_file_input_output.txt", 'a') as file:
     txt = file.write("qwerty!!!")
     file.close()
  
+# Read file
 file_read = open("text_file_input_output.txt", 'r')
 print(file_read.read())
